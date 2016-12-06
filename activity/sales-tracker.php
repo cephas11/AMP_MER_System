cbb43473493cc4378136a4fc789b4fad05474708
Nana Aba <abaodum@gmail.com>
2016-12-06 21:48:37 +0000

    sales tracker modified

--- activity/sales-tracker.php
+++ activity/sales-tracker.php
@@ -39,14 +39,7 @@
                             <div class="card-header">
                                 <strong> Beneficiaries List</strong>
                                 <div class="row">
-                                    <div class="col-lg-12">
-                                        <div class="pull-right">
-                                            <a class="btn btn-primary "href="beneficiary-form" >New Beneficiary</a>
-                                            <a  class="btn btn-primary" href="bulk-beneficiary-upload" >Bulk Upload</a>
-
-                                        </div>
-
-                                    </div>
+                             
                                 </div>
                             </div>
                             <div class="row">
