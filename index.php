ee12cf2ce2849835f09ef7bd5427ddd16a93359b
Nana Aba <abaodum@gmail.com>
2016-12-07 19:36:04 +0000

    login page customization

--- index.php
+++ index.php
@@ -1,64 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Log In -USAD Portal</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
-    <meta name="description" content="Mer System is a web protal to track farmers activities">
-    
-    <meta property="og:type" content="website">
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:site" content="@naksoid">
-    <meta name="twitter:creator" content="@naksoid">
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="manifest.json">
-    <link rel="mask-icon" href="safari-pinned-tab.svg" color="#27ae60">
-    <meta name="theme-color" content="#ffffff">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,400italic,500,700">
-    <link rel="stylesheet" href="css/vendor.min.css">
-    <link rel="stylesheet" href="css/elephant.min.css">
-    <link rel="stylesheet" href="css/login-3.min.css">
-  </head>
-  <body>
-    <div class="login">
-      <div class="login-body">
-        <a class="login-brand" href="#">
-            <img class="img-responsive" src="img/logo.png" height="30" width="60" alt="USAD">
-               <img class="img-responsive" src="img/logo2.png" height="30" width="70" alt="USAD">
-        </a>
-        <h3 class="login-heading">Sign in</h3>
-        <div class="login-form">
-            <form data-toggle="md-validator" action="beneficiary/beneficiary-form.php">
-            <div class="md-form-group md-label-floating">
-              <input class="md-form-control" type="text" name="username" spellcheck="false" autocomplete="off" data-msg-required="Please enter username." required>
-              <label class="md-control-label">Username</label>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Log In -USAD Portal</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+        <meta name="description" content="Mer System is a web protal to track farmers activities">
+
+        <meta property="og:type" content="website">
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:site" content="@naksoid">
+        <meta name="twitter:creator" content="@naksoid">
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="manifest.json">
+        <link rel="mask-icon" href="safari-pinned-tab.svg" color="#27ae60">
+        <meta name="theme-color" content="#ffffff">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,400italic,500,700">
+        <link rel="stylesheet" href="css/vendor.min.css">
+        <link rel="stylesheet" href="css/elephant.min.css">
+        <link rel="stylesheet" href="css/login-3.min.css">
+        <link rel="stylesheet" href="css/logincustom.css">
+    </head>
+    <body>
+
+        <ul class="slideshow" style=":after {content: '';background: transparent url('img/background.jpg') repeat top left};">
+            <li><span style="background-image: url('img/background.jpg')"></span></li>
+            <li><span style="background-image: url('img/background2.jpg')"></span></li>
+            <li><span style="background-image: url('img/background3.jpg')"></span></li>
+            <li><span style="background-image: url('img/background.jpg')"></span></li>
+            <li><span style="background-image: url('img/background2.jpg')"></span></li>
+            <li><span style="background-image: url('img/background3.jpg')"></span></li>
+
+        </ul>
+
+        <div class="login">
+            <div class="login-body">
+                <a class="login-brand" href="#">
+                    <img class="img-responsive" src="img/logo.png" height="30" width="60" alt="USAD">
+                    <img class="img-responsive" src="img/logo2.png" height="30" width="70" alt="USAD">
+                </a>
+                <h3 class="login-heading">Sign in</h3>
+                <div class="login-form">
+                    <form data-toggle="md-validator" action="beneficiary/beneficiary-form.php">
+                        <div class="md-form-group md-label-floating">
+                            <input class="md-form-control" type="text" name="username" spellcheck="false" autocomplete="off" data-msg-required="Please enter username." required>
+                            <label class="md-control-label">Username</label>
+                        </div>
+                        <div class="md-form-group md-label-floating">
+                            <input class="md-form-control" type="password" name="password" minlength="6" data-msg-minlength="Password must be 6 characters or more." data-msg-required="Please enter your password." required>
+                            <label class="md-control-label">Password</label>
+                        </div>
+                        <div class="md-form-group md-custom-controls">
+                            <label class="custom-control custom-control-primary custom-checkbox">
+                                <input class="custom-control-input" type="checkbox" checked="checked">
+                                <span class="custom-control-indicator"></span>
+                                <span class="custom-control-label">Keep me signed in</span>
+                            </label>
+                            <span aria-hidden="true"> · </span>
+                            <a href="#">Forgot password?</a>
+                        </div>
+                        <button class="btn btn-primary btn-block" type="submit">Sign in</button>
+                    </form>
+                </div>
             </div>
-            <div class="md-form-group md-label-floating">
-              <input class="md-form-control" type="password" name="password" minlength="6" data-msg-minlength="Password must be 6 characters or more." data-msg-required="Please enter your password." required>
-              <label class="md-control-label">Password</label>
-            </div>
-            <div class="md-form-group md-custom-controls">
-              <label class="custom-control custom-control-primary custom-checkbox">
-                <input class="custom-control-input" type="checkbox" checked="checked">
-                <span class="custom-control-indicator"></span>
-                <span class="custom-control-label">Keep me signed in</span>
-              </label>
-              <span aria-hidden="true"> · </span>
-              <a href="#">Forgot password?</a>
-            </div>
-            <button class="btn btn-primary btn-block" type="submit">Sign in</button>
-          </form>
+
         </div>
-      </div>
-     
-    </div>
-    <script src="js/vendor.min.js"></script>
-    <script src="js/elephant.min.js"></script>
-   
-  </body>
+        <script src="js/vendor.min.js"></script>
+        <script src="js/elephant.min.js"></script>
+
+    </body>
 </html>
 <!-- Localized -->
 
